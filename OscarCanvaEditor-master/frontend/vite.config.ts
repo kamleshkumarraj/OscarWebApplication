afbc04194dc476e9c01788d54f672bc2c7592b54
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2024-10-03 14:53:40 +0530

    nothing

--- OscarCanvaEditor-master/frontend/vite.config.ts
+++ OscarCanvaEditor-master/frontend/vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: { },
- 
+  build: {
+    rollupOptions: {
+      external: ['@emotion/react/jsx-runtime']
+    }
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
